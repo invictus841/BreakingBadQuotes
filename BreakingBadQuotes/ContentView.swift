46118fb09ca4539295bd0054b3da0e12a475feda
NT <usedforfunny@gmail.com>
2023-01-16 22:15:18 +0100

    1.0

--- BreakingBadQuotes/ContentView.swift
+++ BreakingBadQuotes/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var quotes = [Quote]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+            NavigationStack {
+                
+                ZStack {
+                    Color("Laurel Green")
+                        .ignoresSafeArea()
+                    
+                    ScrollView {
+                        VStack {
+                            ForEach(quotes, id: \.quote) { quote in
+                                VStack {
+                                    Text(quote.author)
+                                        .font(.headline)
+                                        .foregroundColor(Color("English Violet"))
+                                    Text(quote.quote)
+                                        .font(.body)
+                                        .foregroundColor(.black.opacity(0.5))
+                                }
+                                .padding()
+                            }
+                           
+                        }
+                    }
+                    Button("Refresh!") {
+                        fetchDataAgain()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                }
+                .navigationTitle("Breaking Bad Quotes")
+                .task {
+                    await fetchData()
+                }
+            }
+        
+    }
+    
+    func fetchDataAgain() {
+        Task {
+            await fetchData()
         }
-        .padding()
+    }
+    
+    func fetchData() async {
+        // create url
+        guard let url = URL(string: "https://api.breakingbadquotes.xyz/v1/quotes/15")
+        else {
+            print("This url does not seem to work.")
+            return
+        }
+        
+        // fetch data from url
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // decode fetched data
+            if let decodedResponse = try? JSONDecoder().decode([Quote].self, from: data) {
+                quotes = decodedResponse
+            }
+        } catch {
+            print("Walter is not happy cause this data is not valid.")
+        }
     }
 }
 
---
+++ BreakingBadQuotes/QuoteModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuoteModel.swift
+//  BreakingBadQuotes
+//
+//  Created by Alexandre Talatinian on 16/01/2023.
+//
+
+import Foundation
+
+struct Quote: Codable {
+//    var id: UUID
+    var author: String
+    var quote: String
+}
