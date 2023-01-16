c542bf0ca1ba1878dc372d0e3aef86cc5873c103
NT <usedforfunny@gmail.com>
2023-01-16 16:51:29 +0100

    Initial Commit

---
+++ BreakingBadQuotes/BreakingBadQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadQuotesApp.swift
+//  BreakingBadQuotes
+//
+//  Created by Alexandre Talatinian on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
